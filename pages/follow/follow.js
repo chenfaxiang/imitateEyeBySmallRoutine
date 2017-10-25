f93c712a7670212f04e524c7361607a70227f02e
1061706520@qq.com <1061706520@qq.com>
2017-10-25 14:36:28 +0800

    add the follow page dom and data

--- pages/follow/follow.js
+++ pages/follow/follow.js
@@ -2,9 +2,47 @@ const app = getApp();
 
 Page({
   data: {
-    followData: '关注页面数据'
+    followData: '关注页面数据',
+    followedData: [
+      {
+        followImg: 'http://img.kaiyanapp.com/9bbe8f164606b493274152c516b3fbe5.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/a52ebf1b9bc673561569ec60155968a7.png?imageMogr2/quality/60/format/jpg',
+        followIntroTitle: '五分钟学会弹奏周杰伦情歌',
+        followIntroBrand: '九零加 / #音乐 /'
+      },
+      {
+        followImg: 'http://img.kaiyanapp.com/51a0caff5edb03ba154c3996c0cc6f7b.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/70716e90178d2ea35be9e550c26795f1.jpeg',
+        followIntroTitle: '来一场炫酷的狩猎吧……',
+        followIntroBrand: '野外生存 / #探险 /'
+      },
+      {
+        followImg: 'http://img.kaiyanapp.com/9bbe8f164606b493274152c516b3fbe5.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/a52ebf1b9bc673561569ec60155968a7.png?imageMogr2/quality/60/format/jpg',
+        followIntroTitle: '五分钟学会弹奏周杰伦情歌',
+        followIntroBrand: '九零加 / #音乐 /'
+      },
+      {
+        followImg: 'http://img.kaiyanapp.com/51a0caff5edb03ba154c3996c0cc6f7b.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/70716e90178d2ea35be9e550c26795f1.jpeg',
+        followIntroTitle: '来一场炫酷的狩猎吧……',
+        followIntroBrand: '野外生存 / #探险 /'
+      },
+      {
+        followImg: 'http://img.kaiyanapp.com/9bbe8f164606b493274152c516b3fbe5.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/a52ebf1b9bc673561569ec60155968a7.png?imageMogr2/quality/60/format/jpg',
+        followIntroTitle: '五分钟学会弹奏周杰伦情歌',
+        followIntroBrand: '九零加 / #音乐 /'
+      },
+      {
+        followImg: 'http://img.kaiyanapp.com/51a0caff5edb03ba154c3996c0cc6f7b.jpeg?imageMogr2/quality/60/format/jpg',
+        followIcon: 'http://img.kaiyanapp.com/70716e90178d2ea35be9e550c26795f1.jpeg',
+        followIntroTitle: '来一场炫酷的狩猎吧……',
+        followIntroBrand: '野外生存 / #探险 /'
+      }
+    ]
   },
   onLoad: function () {
-    console.log('----follow pages');
+
   }
 });
